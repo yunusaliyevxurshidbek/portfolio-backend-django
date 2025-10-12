4c91554a3b1632467d29d0c78a65094147b9dc65
Xurshidbek Yunusaliyev <xurshidbek4466@gmail.com>
2025-10-12 21:57:50 +0500

    testing 7

--- portfolio/base/views.py
+++ portfolio/base/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import boto3, os
 
-# Create your views here.
+def test_r2_upload(request):
+    try:
+        session = boto3.session.Session()
+        s3 = session.client(
+            service_name="s3",
+            aws_access_key_id=os.getenv("R2_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("R2_SECRET_ACCESS_KEY"),
+            endpoint_url=os.getenv("R2_ENDPOINT"),
+        )
+
+        s3.put_object(
+            Bucket=os.getenv("R2_BUCKET_NAME"),
+            Key="test_upload/hello_from_django_via_view.txt",
+            Body=b"Hello from Django via HTTP view!"
+        )
+        return JsonResponse({"status": "success", "message": "File uploaded to R2 successfully!"})
+    except Exception as e:
+        return JsonResponse({"status": "error", "message": str(e)})
